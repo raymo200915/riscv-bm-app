eb75c6c2b41d2016eb17e795f5e6056f0d1b095e
Raymond Mao <raymond.mao@riscstar.com>
2025-12-08 10:44:17 -0800

    Update bm_app and make infrastructure
    Update the application to be built under M-mode and S-mode.
    Update the README for guide and instructions.
    Build artifacts are tested via Buildroot RISC-V project.
    
    Signed-off-by: Raymond Mao <raymond.mao@riscstar.com>

--- bm_app.c
+++ bm_app.c
@@ -1,48 +1,56 @@
 #include <stdint.h>
 
-static inline long sbi_call(long ext, long fid,
-			    long arg0, long arg1,
-			    long arg2, long arg3)
-{
-	(void)arg1;
+#ifdef BM_S_MODE
 
-	register long a0 asm("a0") = arg0;
-	register long a1 asm("a1") = 0;
-	register long a2 asm("a2") = arg2;
-	register long a3 asm("a3") = arg3;
-	register long a6 asm("a6") = fid;
-	register long a7 asm("a7") = ext;
+#define SBI_EXT_CONSOLE_PUTCHAR 1UL
+
+static inline void putchar(char ch)
+{
+	register unsigned long a0 asm("a0") = (unsigned long)(uint8_t)ch;
+	register unsigned long a7 asm("a7") = SBI_EXT_CONSOLE_PUTCHAR;
 
 	asm volatile ("ecall"
-		      : "+r"(a0), "+r"(a1)
-		      : "r"(a2), "r"(a3), "r"(a6), "r"(a7)
+		      : "+r"(a0)
+		      : "r"(a7)
 		      : "memory");
-	return a0;
 }
 
-#define SBI_EXT_CONSOLE_PUTCHAR 0x01
+#else /* via UART MMIO in M-mode */
 
-static void sbi_putc(char ch)
+#define UART0_BASE      0x10000000UL
+#define UART_RBR        0
+#define UART_THR        0
+#define UART_LSR        5
+#define UART_LSR_THRE   (1 << 5)
+
+static inline void putchar(char ch)
 {
-	sbi_call(SBI_EXT_CONSOLE_PUTCHAR, 0, ch, 0, 0, 0);
+	volatile uint8_t *uart = (volatile uint8_t *)UART0_BASE;
+
+	while (!(uart[UART_LSR] & UART_LSR_THRE))
+		;
+
+	uart[UART_THR] = (uint8_t)ch;
 }
+
+#endif
 
 static void print(const char *s)
 {
 	while (*s) {
-		sbi_putc(*s++);
+		if (*s == '\n')
+			putchar('\r');
+		putchar(*s++);
 	}
 }
 
-void _start(void)
+void main(void)
 {
-	print("\r\n======================================\r\n");
-	print(" Welcome to OpenSBI bare-metal app!\r\n");
-	print(" Running in its own payload \r\n");
-	print("======================================\r\n\r\n");
+	print("\n[BM] main() reached!\n");
+	print(" Welcome to OpenSBI bare-metal app!\n");
+	print(" Running in its own payload\n\n");
 
 	while (1) {
-		asm volatile ("wfi");
+		__asm__ volatile ("wfi");
 	}
 }
-
